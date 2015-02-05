a32b0842578429b60386d54189456a6f246b7321
Hilary Lee <hilary@digintent.com>
2015-02-04 20:37:30 -0600

    Add hide, show

--- client/views/upcoming.js
+++ client/views/upcoming.js
@@ -2,4 +2,22 @@ Template.upcoming.rendered = function() {
 	$('.day').draggable();
 	$('.forecast-container .col-md-5').draggable();
 	$('.forecast-container .col-md-4').draggable();
+
+	$('.day').dblclick(function() {
+		$(this).hide('shake');
+	});
+
+	$('.forecast-container .col-md-5').dblclick(function() {
+		$(this).hide('shake');
+	});
+
+	$('.forecast-container .col-md-4').dblclick(function() {
+		$(this).hide('shake');
+	});
+
+	$('.navbar').dblclick(function() {
+		$('.day').show('puff');
+		$('.forecast-container .col-md-5').show('puff');
+		$('.forecast-container .col-md-4').show('puff');
+	});
 };
