260c3c8b0be2d123a90308f5d9c9aa61748df99e
Hilary Lee <hilary@digintent.com>
2015-02-13 15:22:14 -0600

    Improve d3 graphs.

--- client/views/precip.js
+++ client/views/precip.js
@@ -4,36 +4,58 @@ Template.precip.rendered = function() {
 		var precipHourArray = data.fData.precipHour;
 		var precipDayArray = data.fData.precipDay;
 		var d3Draw = function(precipArray) {
-			console.log(precipArray);
-			console.log(precipArray.length);
+			if (precipArray.length === 49) {
+				precipArray = $.map(precipArray, function(n, i) {
+					if (i <= 13) {
+						return n;
+					}
+				});
+			}
+			else {
+				precipArray = $.map(precipArray, function(n, i) {
+					if (i == 1 || i % 10 == 0) {
+						return n;
+					}
+				});
+			}
 
 			var svg = d3.select('.precip-box').append('svg');
-			var w = 400;
+			var w = 425;
 			var h = 200;
-			var padding = 40;
-			var xScale = d3.scale.linear().domain([0, precipArray.length]).range([padding, w-padding]);
-            var yScale = d3.scale.linear().domain([-0.1, 1]).range([h - padding, padding]);
-            var xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(5);
-            var yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(5);
+			var paddingW = 55;
+			var paddingH = 20;
+			var xScale = d3.scale.linear().domain([0, precipArray.length]).range([paddingW, w-paddingW]);
+            var yScale = d3.scale.linear().domain([-0.1, 1]).range([h - paddingH, paddingH]);
+
+            if (precipArray.length === 8) {
+            	var xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(7).tickFormat(function(d, i) { 
+            				return moment().add(i * 10, 'minutes').format('h:mm');	
+            		});
+            }
+            else {
+            	var xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(8).tickFormat(function(d, i) {
+            		return moment().add(i, 'hours').format('h a');
+            	});
+            }
+
+            var yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(5).tickFormat(d3.format("%"));
             var lineFunction = d3.svg.line()
             	.x(function(d, i) {return xScale(i);})
             	.y(function(d) {return yScale(d.precipProbability);})
             	.interpolate("basis");
 
-            svg.attr("width", w);
+            svg.attr("width", w + 25);
             svg.attr("height", h);
 
             svg.append("path").attr("d", lineFunction(precipArray)).attr("stroke", "white").attr("stroke-width", 2).attr("fill", "none");
-            svg.append("g").attr("class", "axis").attr("transform", "translate(0," + (h - padding) + ")").call(xAxis);
-            svg.append("g").attr("class", "axis").attr("transform", "translate(" + padding + ",0)").call(yAxis);
+            svg.append("g").attr("class", "axis").attr("transform", "translate(0," + (h - paddingH) + ")").call(xAxis);
+            svg.append("g").attr("class", "axis").attr("transform", "translate(" + paddingW + ",0)").call(yAxis);
 		};
 
 		$('svg').remove();
 		d3Draw(precipHourArray);
 		$('svg').after('</br>');
 		d3Draw(precipDayArray);
-		var currentTime = moment();
-		console.log(currentTime);
 
 	});
 	};
